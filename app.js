025562d4058d62a640872a8ec153ea203a3d2d63
Thomas Birdwell <thomas.y.birdwell@gmail.com>
2021-09-12 15:01:30 -0500

    Project Completed

--- app.js
+++ app.js
@@ -1,49 +1,17 @@
-const express = require('express');
-const morgan = require('morgan');
-const path = require('path');
+const express = require("express");
+const morgan = require("morgan");
+const postBank = require("./postBank")
+
 
 const app = express();
-
-const postBank = require('./postBank.js');
-
-app.use(morgan('dev'));
 app.use(express.static('public'));
+app.use(morgan('dev'));
 
-app.get("/", (req, res)=> {
+app.get("/", (req, res) => {
     const posts = postBank.list();
-
+        
     const html = `<!DOCTYPE html>
     <html>
-      <head>
-        <title>Wizard News</title>
-        <link rel="stylesheet" href="/style.css" />
-      </head>
-      <body>
-        <div class="news-list">
-          <header><img src="/logo.png"/>Wizard News</header>
-          ${posts.map(post => `
-            <div class='news-item'>
-              <p>
-                <span class="news-position">${post.id}. ▲</span>${post.title}
-                <small>(by ${post.name})</small>
-              </p>
-              <small class="news-info">
-                ${post.upvotes} upvotes | ${post.date}
-              </small>
-            </div>`
-          ).join('')}
-        </div>
-      </body>
-      </html>`
-    res.send(html);
-});
-
-app.get('/post/:id', (req, res) => {
-  const post = postBank.find(+req.params.id);
-  console.log(post.id);
-
-  const html = `<!DOCTYPE html>
-  <html>
     <head>
       <title>Wizard News</title>
       <link rel="stylesheet" href="/style.css" />
@@ -51,37 +19,10 @@ app.get('/post/:id', (req, res) => {
     <body>
       <div class="news-list">
         <header><img src="/logo.png"/>Wizard News</header>
+        ${posts.map(post => `
           <div class='news-item'>
             <p>
-              <span class="news-position">${post.id}. ▲</span>${post.title}
-              <small>(by ${post.name})</small>
-            </p>
-            <small class="news-info">
-              ${post.upvotes} upvotes | ${post.date}
-            </small>
-          </div>
-      </div>
-    </body>
-    </html>`
-  res.send(html);
-});
-
-app.get('/post/:name', (req, res) => {
-  const postName = postBank.findByName(req.params.name);
-
-  const html = `<!DOCTYPE html>
-  <html>
-    <head>
-      <title>Wizard News</title>
-      <link rel="stylesheet" href="/style.css" />
-    </head>
-    <body>
-      <div class="news-list">
-        <header><img src="/logo.png"/>Wizard News</header>
-        ${postName.map(post => `
-          <div class='news-item'>
-            <p>
-              <span class="news-position">${post.id}. ▲</span>${post.title}
+              <span class="news-position">${post.id}. ▲</span><a href="/posts/${post.id}">${post.title}</a>
               <small>(by ${post.name})</small>
             </p>
             <small class="news-info">
@@ -91,13 +32,65 @@ app.get('/post/:name', (req, res) => {
         ).join('')}
       </div>
     </body>
-    </html>`
-  res.send(html);
+  </html>`;
+
+
+    res.send(html);
 
 });
 
-const PORT = 1337;
+app.get('/posts/:id', (req, res) => {
+    const id = req.params.id;
+    const posts = postBank.list();
+    const post = postBank.find(+id);
+        if (!post.id) {
+            res.status(404)
+            const html = `
+            <!DOCTYPE html>
+            <html>
+            <head>
+              <title>Wizard News</title>
+              <link rel="stylesheet" href="/style.css" />
+            </head>
+            <body>
+              <header><img src="/logo.png"/>Wizard News</header>
+              <div class="not-found">
+                <p>Accio Page! 🧙‍♀️ ... Page Not Found</p>
+              </div>
+            </body>
+            </html>`
+            res.send(html)
+        } else  {
+    const html = `<!DOCTYPE html>
+    <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <div class="news-list">
+        <header><img src="/logo.png"/>Wizard News</header>
+          <div class='news-item'>
+            <p>
+              <span class="news-position">${post.id}. ▲</span>${post.title}
+              <small>(by ${post.name})</small>
+            </p>
+            <small class="news-info">
+              ${post.upvotes} upvotes | ${post.date}
+            </small>
+            <p>${post.content}</p>
+          </div>
+      </div>
+    </body>
+  </html>`;
+  res.send(html);
+        }
+});
+
+
+  const PORT = 1337;
 
 app.listen(PORT, ()=> {
     console.log(`App listening in port ${PORT}`);
 });
+
